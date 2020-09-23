909536b8b4765d2f39ce084b18d98a6f186caa29
Scott MacDonald <scott@smacdo.com>
2020-09-22 21:05:01 -0700

    A much nicer install script

--- setup.sh
+++ setup.sh
@@ -4,16 +4,24 @@
 #  - This script will set up common directories, dot files and other system
 #    settings
 #############################################################################
-bin_dir=$HOME/bin
+
+# Bail out if this doesn't look like the checkout directory.
+if [[ ! -f "./setup.sh" ]] || [[ ! -f "./README" ]]; then
+  echo "Please run this script from the root directory of the git checkout"
+  exit 1
+fi
+
+# Ask the user for confirmation before continuing.
+checkout_dir=$PWD
 
 echo "This script will automatically update your system to use common directories, "
 echo "various dotfiles (zsh, vimrc, etc) and other assorted goodies. "
 echo " "
+echo "The current directory is: $checkout_dir"
+echo " "
 
-# Ask the user to confirm before proceeding
-read -p "Do you wish to continue? (y|n) "
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+read -p "Is this the root directory of the git checkout? (y|n) "
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
 
@@ -23,44 +31,88 @@ echo "Creating common directories..."
 mkdir -vp $HOME/.zsh_local
 mkdir -vp $HOME/.vim_runtime/backups
 mkdir -vp $HOME/.vim_runtime/tmp
-mkdir -vp $HOME/projects
+
+# Safely symlink file
+safe_symlink()
+{
+    SOURCE=$1
+    TARGET=$2
+
+    # Make sure source and target were given.
+    if [ $# -lt 2 ]; then
+        echo "ERROR: Missing source and or target arguments"
+        exit 1
+    fi
+
+    if [ -z "$TARGET" ]; then
+        echo "ERROR: Target argument not given or empty"
+        exit 1
+    fi
+
+    if [ -z "$SOURCE" ]; then
+        echo "ERROR: Source argument not given or empty"
+        exit 1
+    fi
+
+    # Check if target already exists before symlinking.
+    if [ ! -f "$TARGET" ] && [ ! -d "$TARGET" ]; then
+        # Target does not exist, go ahead and symlink.
+        echo "Symlinking $TARGET => $SOURCE"
+        ln -s $SOURCE $TARGET
+    else
+        # Target file exists. Check for these conditions:
+        #  1. Is Symlink => Skip if symlink points to $SOURCE, else error.
+        #  2. Is regular file => Back it up to $TARGET.bak (if $TARGET.bak does not exist).
+        #  3. Else => Error.
+        local FOUND_CONDITION=0
+
+        if [ "$(readlink -- "$TARGET")" = "$SOURCE" ]; then
+            echo "$TARGET is already symlinked to $SOURCE."
+            FOUND_CONDITION=1
+        fi
+
+        if [ ! -e "$TARGET.bak" ] && [ $FOUND_CONDITION -eq 0 ]; then
+            # Ask user for permission.
+            read -p "Can I rename $TARGET to $TARGET.bak before symlinking? (y|n) "
+            if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+                exit 1
+            fi
+
+            # Rename existing target file.
+            echo "Renaming $TARGET to $TARGET.bak"
+            mv $TARGET $TARGET.bak
+
+            # Now symlink target.
+            echo "Creating symlink $TARGET => $SOURCE"
+            ln -s $SOURCE $TARGET
+
+            FOUND_CONDITION=1
+        fi
+
+        # If we couldn't handle the $TARGET existing by this point, just error.
+        if [ $FOUND_CONDITION -eq 0 ]; then
+            echo "ERROR: $TARGET already exists. Please remove it and re-run the script".
+            exit 1
+        fi
+    fi
+}
 
 # Symlink useful dotfiles
-echo "Copying dotfiles..."
+echo "Symlinking dotfiles..."
 
-rm -i $HOME/.vimrc
-ln -sv $bin_dir/.vimrc $HOME
-
-rm -ir $HOME/.vim
-ln -sv $bin_dir/.vim $HOME
-
-rm -i $HOME/.bash_profile
-ln -sv $bin_dir/.bash_profile $HOME
-
-rm -i $HOME/.bashrc
-ln -sv $bin_dir/.bashrc $HOME
-
-rm -i $HOME/.zshrc
-ln -sv $bin_dir/.zshrc $HOME
-
-rm -ir $HOME/.zsh
-ln -sv -T $bin_dir/zsh_files $HOME/.zsh
-
-rm -i $HOME/.dircolors
-ln -sv $bin_dir/.dircolors $HOME
-
-rm -i $HOME/.inputrc
-ln -sv $bin_dir/.inputrc $HOME
+safe_symlink "$checkout_dir/.vimrc" "$HOME/.vimrc"
+safe_symlink "$checkout_dir/.vim" "$HOME/.vim"
+safe_symlink "$checkout_dir/.bash_profile" "$HOME/.bash_profile"
+safe_symlink "$checkout_dir/.bashrc" "$HOME/.bashrc"
+safe_symlink "$checkout_dir/.zshrc" "$HOME/.zshrc"
+safe_symlink "$checkout_dir/zsh_files" "$HOME/.zsh"
+safe_symlink "$checkout_dir/.dircolors" "$HOME/.dircolors"
+safe_symlink "$checkout_dir/.inputrc" "$HOME/.inputrc"
 
 # Install fonts
-echo "Installing liberation fonts..."
-mkdir -pv $HOME/.fonts/liberation
-cp $bin_dir/fonts/liberation/*.ttf $HOME/.fonts/liberation
+# TODO: Make this a configurable option.
+mkdir -pv $HOME/.fonts/
 
-echo "Installing ubuntu fonts..."
-mkdir -pv $HOME/.fonts/ubuntu
-cp $bin_dir/fonts/ubuntu/*.ttf $HOME/.fonts/ubuntu
-
-echo "Installing consola fonts..."
-mkdir -pv $HOME/.fonts/consola
-cp $bin_dir/fonts/consola/*.ttf $HOME/.fonts/consola
+safe_symlink "$checkout_dir/fonts/liberation" "$HOME/.fonts/liberation"
+safe_symlink "$checkout_dir/fonts/ubuntu" "$HOME/.fonts/ubuntu"
+safe_symlink "$checkout_dir/fonts/consola" "$HOME/.fonts/consola"
