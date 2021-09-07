3c1cf5b11ac1be45441a3746a70ba6a8e9ad4296
Scott MacDonald <scott@smacdo.com>
2021-09-06 21:40:55 -0700

    Adding directory for post install setup scripts

---
+++ postinstall/debian-install.sh
@@ -0,0 +1,26 @@
+##
+# TODO
+# - Add contrib and non-free 
+
+# apt update
+
+# unattended
+# apt install unattended-upgrades apt-listchanges 
+
+
+# Install core packages
+#  tmux
+#  vim
+#  curl
+
+# lwsh
+
+# (opt) install build packages for C++
+#  build-essential binutils
+#  clang, cmake, lldb, ..
+
+# Generate ssh config and keys
+
+# etc keeper
+
+# htop 
