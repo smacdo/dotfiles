94a78322d3d743017b6e0faffbcf20e1fff45414
Scott MacDonald <scott@smacdo.com>
2021-10-26 18:12:30 -0700

    Adding improved kill word function in zsh for paths

--- zsh_files/functions.zsh
+++ zsh_files/functions.zsh
@@ -1,1 +1,16 @@
 # Shared function should go in .dotfiles/.functions. ZSH specific functions go here.
+
+################################################################################
+# A variation on ZSH's default "kill-word" command that treats path separator
+# '/' as a word boundary.
+# Ref: https://unix.stackexchange.com/a/319854
+################################################################################
+backward-kill-dir () {
+    local WORDCHARS=${WORDCHARS/\/}
+    zle backward-kill-word
+    zle -f kill
+}
+
+# Bind the nicer kill word command to ALT + BACKSPACE.
+zle -N backward-kill-dir
+bindkey '^[^?' backward-kill-dir
