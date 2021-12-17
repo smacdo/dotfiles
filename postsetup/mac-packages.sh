301da79147d5fc2cb786bd2d33efaa8c2f516ee7
Scott MacDonald <scott@smacdo.com>
2021-12-16 22:00:20 -0800

    Mac packages update

--- postsetup/mac-packages.sh
+++ postsetup/mac-packages.sh
@@ -2,6 +2,17 @@
 # Author: Scott MacDonald
 # Purpose: Installs Homebrew along with commonly used applications.
 #===============================================================================
+
+################################################################################
+#
+################################################################################
+check_brew_ok() {
+  brew doctor && return 0
+  # TODO: Prompt user to continue with the issues listed.
+  echo "Please address brew issues before re-running this script"
+  exit 1
+}
+
 if [[ ! is_osx ]]; then
     echo "This script only works on MacOS boxes."
     return 1
@@ -21,21 +32,32 @@ if [[ ! "$(type -p brew)" ]]; then
     return 1
 fi
 
+# Check for any brew issues before continuing onward to installing packages.
+# TODO: Add command line option to skip this check.
+
 # Update Homebrew.
 echo "Updating Homebrew..."
 brew doctor
 brew update
 
+# Get brew dir prefix.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities for simpler crossplatform scripts.
+brew install coreutils findutils grep
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+
+# Install a more modern version of bash.
+brew install bash bash-completion@2
+
 # Install common packages for Mac OS
-brew install vim
-brew install htop fzf neovim ripgrep tmux zsh mosh
-brew install zoom
-brew install git git-lfs
+brew install vim git git-lfs htop fzf neovim ripgrep tmux zsh mosh highlight
 
 brew install --cask 1password
 brew install --cask sublime-text
 brew install --cask iterm2
-brew install --cask messenger-native
 
 # also: --cask workplace-chat
 #              microsoft-outlook
@@ -43,3 +65,6 @@ brew install --cask messenger-native
 # Install developer packages for Mac OS
 brew install cmake clang-format
 brew install --cask visual-studio-code
+
+# All done!
+brew cleanup
