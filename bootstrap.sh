ec1cecb71a359f1dd49ad97fc83d7d7cd7089b96
Scott MacDonald <scott@smacdo.com>
2024-05-12 10:37:27 -0700

    add todo for auto register (n)vim plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -93,6 +93,9 @@ main() {
 
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  # TODO: Install plugin manager automaticaly with
+  # <path/to/[n]vim> +'PlugInstall --sync' +qa
 }
 
 ################################################################################
