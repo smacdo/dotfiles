07bd45a46c3858998b63896b2efd2fa5f1d6e25f
Scott MacDonald <root@smacdo.com>
2025-11-29 20:44:30 -0800

    update path handling
     - force path to be added even if it does not exist
     - only load homebrew paths on macos
     - add `.local/bin` as a new path

--- shell_profile/paths.sh
+++ shell_profile/paths.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 # This file configures the $PATH environment variable for a shell session.
-
-################################################################################
+#==============================================================================#
 # Add a new directory to the front of the PATH environment variable if it was
 # not already present.
 #
 # Arguments:
 #  $1: The path to add. If it does not exist it will not be added.
+#  $2 `-f`: add the path even if it does not exist.
 #
 # Ref: https://unix.stackexchange.com/a/124447
-################################################################################
+#==============================================================================#
 add_path_front() {
     if [ -z "$1" ]; then
         echo "ERROR: Missing path to add"
         return 1
     fi
 
-    if [ -d "$1" ]; then
+    if [ -d "$1" ] || [ "${2:-}" = "-f" ]; then
       case ":${PATH:=$1}:" in
           *:"$1":*)   ;;
           *) PATH="$1:$PATH"   ;;
@@ -24,26 +24,30 @@ add_path_front() {
     fi
 }
 
-################################################################################
+#==============================================================================#
 # Add a new directory to the end of the PATH environment variable if it was not
 # already present.
 #
 # Arguments:
 #  $1: The path to add. If it does not exist it will not be added.
-################################################################################
+#  $2 `-f`: add the path even if it does not exist.
+#==============================================================================#
 add_path_back() {
     if [ -z "$1" ]; then
         echo "ERROR: Missing path to add"
         return 1
     fi
     
-    if [ -d "$1" ]; then
+    if [ -d "$1" ] || [ "${2:-}" = "-f" ]; then
       case ":${PATH:=$1}:" in
           *:"$1":*)   ;;
           *) PATH="$PATH:$1"   ;;
       esac
     fi
 }
+
+# Some tools like to install to .local/bin
+add_path_back "$HOME/.local/bin" -f
 
 # The rust tool 'cargo' install binaries to the user's home directory.
 # (prepended in case cargo needs to override system installed rustc).
@@ -52,22 +56,18 @@ add_path_back "$HOME/.cargo/bin"
 # UV installs tools to the user's home directory as well.
 add_path_back "$HOME/.local/share/uv/tools"
 
-# Some tools like to install to .local/bin
-add_path_back "$HOME/.local/bin"
-
-# Support local homebrew installs (~/homebrew)
+# Support default Homebrew installations as well as installations to $HOME.
 #
 # NOTE: The new preferred local dir for homebrew is `.homebrew`. Continue to
 #       support the older directory but only add it if it is detected. Add the
 #       other path by default to allow the case of booting into this profile,
 #       installing Homebrew locally and then commands should "just work".
-if [ -d "$HOME/homebrew/bin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
+  add_path_front "/opt/homebrew/bin"
+  add_path_front "$HOME/.homebrew/bin"
+  add_path_front "$HOME/.homebrew/sbin"
   add_path_front "$HOME/homebrew/bin"
 fi
 
-add_path_front "/opt/homebrew/bin"
-add_path_front "$HOME/.homebrew/bin"
-add_path_front "$HOME/.homebrew/sbin"
-
-# Also support scripts from dotfiles (this will override ~/homebrew).
+# Dotfiles scripts (this will override ~/homebrew).
 add_path_back "$S_DOTFILE_ROOT/bin"
