ac3e68a10a6ba70f7d2c6f3b6e1a6a18d96e489d
Scott MacDonald <root@smacdo.com>
2026-02-25 18:37:08 -0800

    improved wezterm config
      - add nerd font fonts with fallback
      - removed native window decoration (at least for linux)
      - removed margin on window

--- settings/wezterm/wezterm.lua
+++ settings/wezterm/wezterm.lua
@@ -1,7 +1,22 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font_with_fallback {
+  'JetBrainsMono Nerd Font',
+  'JetBrains Mono'
+}
+
 config.color_scheme = 'Catppuccin Mocha'
+
+-- Hide the title bar but keep resizable borders
+config.window_decorations = "RESIZE"
+
+-- Reduce window margins
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
 
 return config
