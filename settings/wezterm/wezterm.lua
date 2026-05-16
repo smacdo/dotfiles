147593cbdd7838bb46c236a817b613581bbfeb12
Scott MacDonald <root@smacdo.com>
2026-05-16 10:04:07 -0700

    adjust wezterm scroll speed

--- settings/wezterm/wezterm.lua
+++ settings/wezterm/wezterm.lua
@@ -1,6 +1,27 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+-- Limit scroll speed to three lines for console. Setting `alt_screen = false`
+-- means tmux and vim behavior is unchanged.
+--
+-- Scroll is set to three lines up/down. wezterm seems to use page up / down
+-- which is imo bizzare.
+config.mouse_bindings = {
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'NONE',
+    action = wezterm.action.ScrollByLine(-3),
+    alt_screen = false,
+  },
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'NONE',
+    action = wezterm.action.ScrollByLine(3),
+    alt_screen = false,
+  }
+}
+
+-- My preferred font and color scheme.
 config.font = wezterm.font_with_fallback {
   'JetBrainsMono Nerd Font',
   'JetBrains Mono'
